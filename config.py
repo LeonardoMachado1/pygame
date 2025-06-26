99a91ef2f8c3baea828aac694b739b6017cd9373
pcanabarro <pedroh.canabarro@gmail.com>
2025-06-26 15:09:16 -0300

    Formatting and creating a config file

---
+++ config.py
@@ -0,0 +1,32 @@
+GRID_SIZE = 20
+CELL_SIZE = 30
+NUM_MINES = 40
+UI_HEIGHT = 60
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+COLOR_HIDDEN = (189, 189, 189)
+COLOR_REVEALED = (224, 224, 224)
+COLOR_BORDER_LIGHT = (240, 240, 240)
+COLOR_BORDER_DARK = (120, 120, 120)
+COLOR_TEXT = (0, 0, 0)
+COLOR_MINE = (255, 255, 0)
+COLOR_FLAG = (244, 67, 54)
+COLOR_BG = (192, 192, 192)
+COLOR_BUTTON = (100, 100, 100)
+COLOR_BUTTON_HOVER = (130, 130, 130)
+
+# Numeros que sao exibidos nas celulas
+NUMBER_COLORS = { 
+    1: (0, 0, 255),
+    2: (0, 128, 0),
+    3: (255, 0, 0),
+    4: (0, 0, 128),
+    5: (128, 0, 0),
+    6: (0, 128, 128),
+    7: (0, 0, 0),
+    8: (128, 128, 128)
+}
+
+CREDITS =  ["Pedro Henrique Canabarro", "Leonardo Pinto Machado", "Felipe Lemos Oliveira", "Everton Santos de Castro"]
